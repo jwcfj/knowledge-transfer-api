77bcad839674a70c4863b7d9d3b9b7b2077e7e9b
jwcfj <jwcfj@gmail.com>
2024-01-19 22:50:08 -0300

    FormularioRepondidoService's GET response structure updated

--- src/main/java/com/knowledgetransfer/api/DTO/ListagemFormularioRespondidoDTO.java
+++ src/main/java/com/knowledgetransfer/api/DTO/ListagemFormularioRespondidoDTO.java
@@ -1,16 +1,24 @@
 package com.knowledgetransfer.api.DTO;
 
 import com.knowledgetransfer.api.model.FormularioRespondido;
+import com.knowledgetransfer.api.model.Questao;
+import com.knowledgetransfer.api.model.RespostaQuestao;
 
 import java.sql.Timestamp;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public record ListagemFormularioRespondidoDTO(
         Long id,
         Timestamp data_criacao,
         String nomeFormulario,
-        String nomeStakeholder
+        String nomeStakeholder,
+        List<ListagemRespostaQuestaoDTO>  respostas_questoes
 ) {
     public ListagemFormularioRespondidoDTO(FormularioRespondido formularioRespondido){
-        this(formularioRespondido.getId(),formularioRespondido.getData_criacao(),formularioRespondido.getFormulario().getNome(),formularioRespondido.getStakeholder().getNome());
+        this(formularioRespondido.getId(),formularioRespondido.getData_criacao(),formularioRespondido.getFormulario().getNome(),formularioRespondido.getStakeholder().getNome(),mapRespostasQuestoes(formularioRespondido.getRespostasQuestoes()) );
+    }
+    private static List<ListagemRespostaQuestaoDTO> mapRespostasQuestoes(List<RespostaQuestao> respostaQuestoes) {
+        return respostaQuestoes.stream().map(ListagemRespostaQuestaoDTO::new).collect(Collectors.toList());
     }
 }
